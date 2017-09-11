de24a8236a2326e2a6992dd47265bed46c8a73d4
antonmontesi <31854902+antonmontesi@users.noreply.github.com>
2017-09-11 18:11:44 +0200

    v3

--- auth0Test.js
+++ auth0Test.js
@@ -1,2 +1,30 @@
+ function loginAuth0()
+    {
+        var form = $("#Auth0Form");
+        form.submit();
+    }
 
-alert('Ciao da me');
+
+    $(document).ready(function()
+    {
+        var form = $("#Auth0Form");
+
+        $("#login") //.... disabilita log in
+
+        if (typeof(Storage) !== "undefined") {
+            var data = localStorage.getItem("antiforgery");
+            //se in cahce 
+        } else {
+        // Sorry! No Web Storage support..
+        }
+
+        $.get( "/_layout/tokenhtml", function( data ) {
+            if (typeof(Storage) !== "undefined") {
+                localStorage.setItem("antiforgery", "data");
+            } 
+            form.append(data);
+            //$("#login"). //abilita login 
+        });
+        
+
+    });
